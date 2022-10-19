287a3239a9ab4b472628693c8d92ff683911da38
Chinazaekpere Ibeh <ibehnaza12@gmail.com>
2022-10-19 16:06:48 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - computes the abso;utr value of an integer
+ * @t: The integer to be computed
+ * Returns: The absolute value
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
